8d78d2fd75d86efbc23bb0f8be2fbce50c856a4c
AlexWetzel <wetzela@live.com>
2017-11-10 12:17:41 -0500

    switched connection to heroku db

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
 var mysql = require ("mysql");
 // local database
-var connection = mysql.createConnection({
-	host:"localhost",
-	port: 3306,
-	user: "root",
-	password: "RootPass",
-	database: "burgers_db"
-});
-
 // var connection = mysql.createConnection({
-// 	host:"us-cdbr-iron-east-05.cleardb.net",
-// 	user: "b0cd511d0a0128",
-// 	password: "42dd8458",
-// 	database: "heroku_87b9b984be4408b"
+// 	host:"localhost",
+// 	port: 3306,
+// 	user: "root",
+// 	password: "RootPass",
+// 	database: "burgers_db"
 // });
+
+var connection = mysql.createConnection({
+	host:"us-cdbr-iron-east-05.cleardb.net",
+	user: "b0cd511d0a0128",
+	password: "42dd8458",
+	database: "heroku_87b9b984be4408b"
+});
 
 connection.connect(function(err) {
   if (err) {
